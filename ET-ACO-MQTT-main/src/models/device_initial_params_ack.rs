2557143c77ae53c92245958570a3a6a969063a7c
Gowtham MA <magowtham7@gmail.com>
2024-11-30 15:57:36 +0530

    fixed bug

--- ET-ACO-MQTT-main/src/models/device_initial_params_ack.rs
+++
@@ -1,8 +0,0 @@
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-#[serde(rename_all = "UPPERCASE")]
-pub struct DeviceInitialParamsAckReq {
-    pub id: String,
-    pub res: String,
-}
--- ET-ACO-MQTT-main/src/models/mod.rs
+++ ET-ACO-MQTT-main/src/models/mod.rs
@@ -1,5 +1,4 @@
 pub mod device;
-pub mod device_initial_params_ack;
 pub mod manifold_refills;
 pub mod manifold_shifts;
 pub mod sensor_alerts;
